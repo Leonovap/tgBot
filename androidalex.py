7ca3559c2b5ac0ae65e69a732771cdd884968063
Alexander <liawonau.al.pa@gmail.com>
2022-05-22 18:54:14 +0300

    1

--- androidalex.py
+++ androidalex.py
@@ -9,10 +9,9 @@ bot = telebot.TeleBot('5266660260:AAFyIXfsv-MosXXPmsECtI8kt95fq-9Iyn0')
 @bot.message_handler(commands=['start'])
 def welcome(message):
     sti = open('wepb/welcome.webp', 'rb')
-    bot.send_sticker(message.chat.id, sti)
+    bot.send_sticker(message.chat.id, sti)-[]
 
-
-bot.send_message(message.chat.id, 'Welcome to the club buddy!')
+    bot.send_message(message.chat.id, 'Welcome to the club buddy!')
 
 
 @bot.message_handler(func=lambda message: True)
